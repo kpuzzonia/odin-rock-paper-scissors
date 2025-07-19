cf1bb9688337082733cea9bb600184c7f2ed8f29
Kari Puzzonia <126693423+kpuzzonia@users.noreply.github.com>
2025-07-19 14:11:24 -0400

    Update game process
    Start game process. Need to improve response to comparison and score incrementation.

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ function getComputerChoice() {
     // generate random number between 1 and 3
     let randomNumber = Math.floor(Math.random() * 3) + 1;
 
-    // if statements to give computer output
+    // if statements to give computer output and return it
     let computerChoice;
     if (randomNumber == 1) {
         return computerChoice = "rock";
@@ -15,4 +15,36 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
+function getHumanChoice () {
+    // gather choice from user and return it
+    let userChoice;
+    userChoice = prompt("Please enter rock, paper, or scissors:" );
+
+    // make sure it is all lower case to help with error handling
+    userChoice = userChoice.toLowerCase();
+    return userChoice;
+}
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log("It's a tie!");
+    }
+    else if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log("Rock beats scissors! You get one point!");
+    }
+    else if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log("Scissors best paper! You get one point!");
+    }
+    else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log("Paper beats rock! You get one point!");
+    }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+let computerChoice = getComputerChoice();
+console.log(computerChoice);
+let humanChoice = getHumanChoice();
+console.log(humanChoice);
+playRound(humanChoice, computerChoice);
