8fa584166618b4b2c117addf7dadc62bffa22db6
Kari Puzzonia <126693423+kpuzzonia@users.noreply.github.com>
2025-07-19 13:29:01 -0400

    Complete step 2 of game set-up

--- game.js
+++ game.js
@@ -1,1 +1,18 @@
-console.log("Hello, World!");
+function getComputerChoice() {
+    // generate random number between 1 and 3
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+
+    // if statements to give computer output
+    let computerChoice;
+    if (randomNumber == 1) {
+        return computerChoice = "rock";
+    }
+    else if (randomNumber == 2) {
+        return computerChoice = "paper";
+    }
+    else {
+        return computerChoice = "scissors"
+    }
+}
+
+console.log(getComputerChoice());
